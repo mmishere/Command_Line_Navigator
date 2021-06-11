1f21cde76cbd50693b11d2d4370763537be3ea24
maaheen <mmaajed2@illinois.edu>
2021-06-11 13:56:18 -0500

    Completed navigator

--- navigator.py
+++ navigator.py
@@ -1,36 +1,26 @@
-from plumbum import cli
-from pyfiglet import Figlet
-# from plumbum.cmd import ls, cd
-from questionary import prompt
-import questionary
-
+from plumbum import cli, local
 from plumbum.cmd import ls, mkdir, touch
-from plumbum import local
-# cd = local["cd"]
-
-
-# possibly also include mkdir and touch, to create new files while on here
+from pyfiglet import Figlet
+from questionary import prompt
+import questionary 
 
 def print_banner(text):
     print(Figlet(font='mini').renderText(text))
 
+
 def get_files():
-    files = ["New file", "New folder", ".."]
+    files = ["New file", "New folder", "Quit", ".."]
     ls_output = ls() # how to include hidden files?
     files += ls_output.split("\n")
     return files
 
+
 def generate_question(files):
     return questionary.rawselect(
-        "Select the desired next location",
-        choices = [{'name': file.strip()} for file in files]
+        "Select the desired next location. Press ctrl+c to exit.",
+        choices = [{'name': file} for file in files]
     )
-    # return [{
-    #     'type': 'rawselect',
-    #     'name': 'file',
-    #     'message': 'Select the desired next location',
-    #     'choices': [{'name': file.strip()} for file in files]
-    # }]
+
 
 class Navigator(cli.Application):
     VERSION = "1.0"
@@ -52,8 +42,14 @@ class Navigator(cli.Application):
             elif (answer == "New folder"):
                 folder_name = input("Input folder name: ")
                 sh.run("mkdir " + folder_name)
+            elif (answer == "Quit"):
+                break
             else:
-                sh.run("cd " + answer)
+                try:
+                    local.cwd.chdir(answer)
+                except:
+                    print("Failed to change directory!")
+
 
 if __name__ == "__main__":
     Navigator()
